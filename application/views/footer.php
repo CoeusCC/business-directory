ef60c6de06a7c8de83bdbceacca660c0c0a1f0b3
Patrick Zeinert <github@coeus.cc>
2012-12-16 17:53:13 -0600

    Default Master View header/footer.
    Used from another project. CSS and JS links will need to be updated

---
+++ application/views/footer.php
@@ -0,0 +1,48 @@
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="<?=site_url();?>js/swipe.min.js"></script>
+        <script src="<?=site_url();?>js/jquery.isotope.min.js"></script>
+        <script src="<?=site_url();?>js/plugins.js"></script>
+        <script src="<?=site_url();?>js/main.js"></script>
+        <script>
+/**            if (Modernizr.touch){
+               window.mySwipe = new Swipe(
+                  document.getElementById('slider')
+               );
+            } else {
+
+            }
+*/
+            $('#container').isotope({
+                itemSelector : '.deal',
+                masonry : {
+                  columnWidth : 120
+                },
+                masonryHorizontal : {
+                  rowHeight: 120
+                },
+                cellsByRow : {
+                  columnWidth : 240,
+                  rowHeight : 240
+                },
+                cellsByColumn : {
+                  columnWidth : 240,
+                  rowHeight : 240
+                }
+            });
+        </script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-34605606-2']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
+    </body>
+</html>
